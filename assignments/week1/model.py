3ced6ea7f8ea6576a968a2b87caa0aa7d97ab3c3
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-01-30 16:41:10 -0500

    Update model.py

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -7,13 +7,15 @@ class LinearRegression:
     b: float
 
     def __init__(self):
-        raise NotImplementedError()
 
     def fit(self, X, y):
-        raise NotImplementedError()
+        params = np.linalg.inv(X.T @ X) @ (X.T @ y)
+        self.w = params[-1]
+        self.b = params[:-1]
 
     def predict(self, X):
-        raise NotImplementedError()
+        reg = torch.matmul(torch.transpose(self.w, 0, -1), X) + self.b
+        return reg
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -21,10 +23,48 @@ class GradientDescentLinearRegression(LinearRegression):
     A linear regression model that uses gradient descent to fit the model.
     """
 
-    def fit(
-        self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
-    ) -> None:
-        raise NotImplementedError()
+    def linear_regression(X, w, b):
+        reg = torch.matmul(torch.transpose(w, 0, -1), X) + b
+        return reg
+
+    def mseloss(y_hat, y):
+        err = torch.mean(torch.square(y_hat - y))
+        return err
+    
+    def gradient_descent(w, b, lr):
+        with torch.no_grad():
+            w -= w.grad * lr
+            b -= b.grad * lr
+            w.grad.zero_()
+            b.grad.zero_()
+        return (w, b)
+    
+    def fit(self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, 
+            epochs: int = 1000) -> None:
+        w = torch.zeros(1, requires_grad=True)
+        b = torch.zeros(1, requires_grad=True)
+
+        losses = []
+
+        epoch_range = trange(num_epochs, desc="loss: ", leave=True)
+        
+        for epoch in epoch_range:
+            if losses:
+                epoch_range.set_description("loss: {:.6f}".format(losses[-1]))
+                epoch_range.refresh()  # to show immediately the update
+
+            y_hat = linear_regression(X, w, b)
+            l = mseloss(y_hat, y_train).mean()
+
+            l.backward()
+            w, b = gradient_descent(w, b, lr)  # Update parameters using their gradient
+
+            losses.append(l)
+
+            time.sleep(0.01)
+        
+        self.w = w
+        self.b = b
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -37,4 +77,4 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        raise NotImplementedError()
+        return linear_regression(X, self.w, self.b)

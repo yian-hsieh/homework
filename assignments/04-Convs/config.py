2c6947c823635e4664bb863496dfed2395adc2fb
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-03-19 00:29:22 -0400

    Update config.py

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -7,12 +7,12 @@ from torchvision.transforms import Compose, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 6
+    num_epochs = 5
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adamax(
-        model.parameters(), lr=0.003, weight_decay=0.0007
+        model.parameters(), lr=0.005, weight_decay=0.0007
     )
 
     transforms = Compose([ToTensor()])

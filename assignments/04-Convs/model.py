bc36a888a385811078ddc726cd8b4b95f26caf52
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-03-19 00:29:00 -0400

    Update model.py

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -3,7 +3,7 @@ import torch
 
 class Model(torch.nn.Module):
     """
-    A convolutional neural network with 5 convolutional layers.
+    A convolutional neural network with 6 convolutional layers.
     """
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
@@ -13,11 +13,14 @@ class Model(torch.nn.Module):
             torch.nn.ReLU(),
             torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, 2),
             torch.nn.BatchNorm2d(64),
             torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(3, 2),
+            torch.nn.BatchNorm2d(128),
             torch.nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
@@ -27,8 +30,10 @@ class Model(torch.nn.Module):
             torch.nn.Flatten(),
             torch.nn.Linear(256 * 5 * 5, 1000),
             torch.nn.ReLU(),
+            torch.nn.Linear(1000, 300),
+            torch.nn.ReLU(),
             torch.nn.Dropout(0.1),
-            torch.nn.Linear(1000, num_classes),
+            torch.nn.Linear(300, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:

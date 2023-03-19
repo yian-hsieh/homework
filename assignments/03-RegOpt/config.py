71e6f3630e7fab84851e3904a773fc45f7cc4e7d
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-03-18 22:35:50 -0400

    Update config.py

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -19,8 +19,8 @@ class CONFIG:
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), 
-        lr=CONFIG.initial_learning_rate, 
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
         weight_decay=CONFIG.initial_weight_decay,
     )
 

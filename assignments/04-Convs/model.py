4997aa07518abbf781b060630a0be45a398c4a28
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-03-18 23:13:22 -0400

    Update model.py

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -2,30 +2,37 @@ import torch
 
 
 class Model(torch.nn.Module):
+    """
+    A convolutional neural network with 5 convolutional layers.
+    """
+    
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(num_channels, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(3, 2),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(3, 1),
-            nn.BatchNorm2d(256),
-            nn.Flatten(),
-            nn.Linear(256 * 5 * 5, 1000),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(1000, num_classes),
+        self.network = torch.nn.Sequential(
+            torch.nn.Conv2d(num_channels, 32, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(3, 2),
+            torch.nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(3, 1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.Flatten(),
+            torch.nn.Linear(256 * 5 * 5, 1000),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.1),
+            torch.nn.Linear(1000, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward function for the neurnal net.
+        """
         return self.network(x)

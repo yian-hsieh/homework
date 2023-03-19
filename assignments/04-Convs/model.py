ca805cba4397ade315564f5cf63ece5b4e7da642
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-03-18 22:25:35 -0400

    Update model.py
    Formatting

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -11,23 +11,21 @@ class Model(torch.nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(64),
-
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(3, 2),
-
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(3, 1),
             nn.BatchNorm2d(256),
-
-            nn.Flatten(), 
-            nn.Linear(256*5*5, 1000),
+            nn.Flatten(),
+            nn.Linear(256 * 5 * 5, 1000),
             nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Linear(1000, num_classes))
+            nn.Linear(1000, num_classes),
+        )
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.network(x)

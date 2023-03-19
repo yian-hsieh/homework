5409a5d19ed3054fb165aa537fbfe4ed17bc54cf
yian-hsieh <104330209+yian-hsieh@users.noreply.github.com>
2023-03-18 22:18:25 -0400

    Update config.py

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -7,7 +7,7 @@ from torchvision.transforms import Compose, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 10
+    num_epochs = 12
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
